dac85b9eea598b1abff7e0ad772c93945ae475cf
JovanStef <jov.box@outlook.com>
2020-06-24 08:06:57 +0200

    user

---
+++ src/Models/user.ts
@@ -0,0 +1,100 @@
+import { EventM } from './event';
+import JWT from 'express-jwt';
+import bcrypt from 'bcrypt';
+
+export interface Email {
+  emailValidator: (email: string) => boolean;
+}
+
+export interface Password {
+  passValidator: (pass: string) => boolean;
+  passHash: (pass: string) => boolean;
+}
+
+export interface Jwt {
+  tokenValidator: (token: string) => boolean;
+  tokenGenerator: (token: string) => boolean;
+}
+
+export class User implements Email, Password, Jwt {
+  private name: string;
+  private email: string;
+  private password: string = '';
+  private tokens: Array<string> = [];
+  private events: Array<EventM> = [];
+
+  constructor(name: string, email: string) {
+    this.name = name;
+    this.email = email;
+  }
+
+  public getName(): string {
+    return this.name;
+  }
+
+  public setName(newName: string): boolean {
+    this.name = newName;
+    return true;
+  }
+
+  public getEmail(): string {
+    return this.email;
+  }
+
+  public setEmail(newEmail: string): boolean {
+    this.email = newEmail;
+    return true;
+  }
+
+  public getPassword(): string {
+    return this.password;
+  }
+
+  public setPassword(newPass: string): boolean {
+    this.password = newPass;
+    return true;
+  }
+
+  public getTokens(): Array<string> {
+    return this.tokens;
+  }
+
+  public addToken(token: string): boolean {
+    this.tokens.push(token);
+    return true;
+  }
+
+  public getEvents(): Array<EventM> {
+    return this.events;
+  }
+
+  public addEvent(eventM: EventM): boolean {
+    this.events.push(eventM);
+    return true;
+  }
+
+  //INTERFACE METHODS
+
+  //email
+  public emailValidator(email: string): boolean {
+    return true;
+  }
+
+  //password
+  public passHash(pass: string): boolean {
+    return true;
+  }
+
+  public passValidator(pass: string): boolean {
+    return true;
+  }
+
+  //token
+  public tokenValidator(token: string): boolean {
+    return true;
+  }
+
+  public tokenGenerator(token: string): boolean {
+    return true;
+  }
+}
--- src/db/models_DB/user_DB.ts
+++ src/db/models_DB/user_DB.ts
@@ -1,46 +1,52 @@
-import mongoose,{Schema,Document} from 'mongoose';
-import EventDB from './event_DB';
+import mongoose, { Schema, Document } from 'mongoose';
 
 export interface User_DB extends mongoose.Document {
-    name: string;
-    email:string;
-    password:string;
-    tokens:Array<string>;
+  name: string;
+  email: string;
+  password: string;
+  tokens: Array<string>;
+  events: Array<string>;
 }
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true,
-        trim: true
+      type: String,
+      required: true,
+      trim: true,
     },
     email: {
-        type: String,
-        unique: true,
-        required: true,
-        trim: true,
-        lowercase: true,
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+      lowercase: true,
     },
     password: {
-        type: String,
-        required: true,
-        trim: true,
+      type: String,
+      required: true,
+      trim: true,
     },
-    tokens: [{
+    tokens: [
+      {
         token: {
-            type: String,
-            required: true
-        }
-    }],
-},{
-    timestamps:true
-}); 
-userSchema.virtual('events',{
-    ref:'EventDB',
-    localField:'_id',
-    foreignField:'user'
-});
-const UserDB = mongoose.model<User_DB>('UserDB',userSchema);
+          type: String,
+          required: true,
+        },
+      },
+    ],
+    events: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'EventDB',
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
-  
+const UserDB = mongoose.model<User_DB>('UserDB', userSchema);
+
 export default UserDB;
